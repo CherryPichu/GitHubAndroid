eebc5ebde63d394d5775e197cc6c024b11486183
CherryPichu <uskawjdu@gmail.com>
2022-11-05 11:17:21 +0900

    Git init

---
+++ SQLite/src/main/java/kr/ac/hallym/githubandroid/MainActivity.kt
@@ -0,0 +1,13 @@
+package kr.ac.hallym.githubandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.hallym.githubandroid.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
